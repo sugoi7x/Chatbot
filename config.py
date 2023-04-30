f8f0397ce6c7e2c462635e9d7c49a936ea67ae09
sugoi7x <104109553+sugoi7x@users.noreply.github.com>
2023-05-01 05:39:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
-OWNER = os.environ.get("OWNER", "kenkanasw")
-GROUP = os.environ.get("GROUP", "musikkugroup")
-CHANNEL = os.environ.get("CHANNEL", "musikkuchannel")
-PROJECT_NAME = os.environ.get("PROJECT_NAME", "PRIVATE CHAT")
+TOKEN = os.environ.get("TOKEN", "6266436808:AAFxngpf-Ujv2_7efBfKbrtXUrnVveChUic")
+ADMIN = int(os.environ.get("ADMIN", "5377369759"))
+OWNER = os.environ.get("OWNER", "Txyzvy")
+GROUP = os.environ.get("GROUP", "xixizxx")
+CHANNEL = os.environ.get("CHANNEL", "xixizxx")
+PROJECT_NAME = os.environ.get("PROJECT_NAME", "Roleplay Chat")
